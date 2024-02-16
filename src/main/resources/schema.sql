e8da92798610d12909b2135c19cc5c9b96674ed9
Januda Bethmin <desilvabethmin@gmail.com>
2024-02-16 17:26:54 +0530

    added schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS "books";
+DROP TABLE IF EXISTS "authors";
+
+CREATE TABLE "authors" (
+    "id" bigint DEFAULT nextval('authors_id_seq') NOT NULL,
+    "name" text,
+    "age" integer,
+    CONSTRAINT "authors_pkey" PRIMARY KEY ("id")
+);
+
+CREATE TABLE "books" (
+    "isbn" text NOT NULL,
+    "title" text,
+    "author_id" bigint,
+    CONSTRAINT "books_okey" PRIMARY KEY ("isbn"),
+    CONSTRAINT "fk_author" PRIMARY KEY (author_id)
+    REFERENCES authors(id)
+);
+
