70b79ad066b45111708c14c1040fbced74a29aad
Januda Bethmin <desilvabethmin@gmail.com>
2024-02-16 21:12:02 +0530

    Added the H2 database dependecy for testing

--- src/main/java/com/springboot/books/dao/impl/BookDaoImpl.java
+++ src/main/java/com/springboot/books/dao/impl/BookDaoImpl.java
@@ -5,7 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 public class BookDaoImpl implements BookDao {
 
-    // We need the JDBC Template here. So, We use dependency injection here. ( From the bean created in the config class )
+    // We need the JDBC Template here. So, We use dependency injection (Autowiring) here. ( From the bean created in the config class )
     // We make this final because to make this cannot be reassigned once it instantiated.
     private final JdbcTemplate jdbcTemplate;
     public BookDaoImpl (final JdbcTemplate jdbcTemplate) {
