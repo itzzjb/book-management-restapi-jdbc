d40b07bc518906a7ed35094661352042aa556f30
Januda Bethmin <desilvabethmin@gmail.com>
2024-02-17 17:04:59 +0530

    read Book classes and tests done

--- src/main/java/com/springboot/books/v1/dao/BookDao.java
+++ src/main/java/com/springboot/books/v1/dao/BookDao.java
@@ -2,7 +2,17 @@ package com.springboot.books.v1.dao;
 
 import com.springboot.books.v1.domain.Book;
 
+import java.util.Optional;
+
 public interface BookDao {
     void create(Book book);
 
+    // Optional is a container either it has something init or don't.
+    // If we try to return an object from the database if the object is not in the database  it returns null.
+    // After we retrieve that object we normally use methods on it.
+    // But we can't call methods on when the event of not finding the object. This results a null pointer exception.
+    // For this situation we use Optionals.
+    // Optional is like a box. We we find the object in the database the object will be put into the box and returned.
+    // Otherwise, only the box will be returned.
+    Optional<Book> findOne(String isbn);
 }
--- src/main/java/com/springboot/books/v1/dao/impl/BookDaoImpl.java
+++ src/main/java/com/springboot/books/v1/dao/impl/BookDaoImpl.java
@@ -3,6 +3,12 @@ package com.springboot.books.v1.dao.impl;
 import com.springboot.books.v1.dao.BookDao;
 import com.springboot.books.v1.domain.Book;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
 public class BookDaoImpl implements BookDao {
 
@@ -20,4 +26,30 @@ public class BookDaoImpl implements BookDao {
         );
     }
 
+    @Override
+    public Optional<Book> findOne(String isbn) {
+        // We get the resulting objects into a list.
+        List<Book> results =  jdbcTemplate.query(
+                "SELECT isbn, title, author_id FROM books WHERE isbn = ? LIMIT 1",
+                new BookRowMapper(),
+                isbn
+        );
+
+        // To find the first optional and return we can use the stream().findFirst()
+        return results.stream().findFirst();
+    }
+
+    // This is a nested class for the RowMapper.
+    // RowMapper creates objects for results we get from the Database.
+    public static class BookRowMapper implements RowMapper<Book> {
+
+        @Override
+        public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return Book.builder()
+                    .isbn(rs.getString("isbn"))
+                    .title(rs.getString("title"))
+                    .authorId(rs.getLong("author_id"))
+                    .build();
+        }
+    }
 }
